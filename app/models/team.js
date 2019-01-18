ec427dc52b5f6b3cb2d9bccacd1691d1d174975f
nathantbissell <nathantbissell@gmail.com>
2019-01-18 07:56:44 -0500

    changed team model

--- app/models/team.js
+++ app/models/team.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const teamSchema = new mongoose.Schema({
+    teamName: {
+        type: String,
+        required: true
+    },
     players: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Player'
